f1a3a7f26bfcfe8220563235f207508b6caf48a8
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-01 20:03:55 +0500

    Add files via upload

---
+++ free_executor_lol.lua
@@ -0,0 +1,100 @@
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.CoreGui
+
+local menu = Instance.new("Frame")
+menu.Size = UDim2.new(0, 400, 0, 300)
+menu.Position = UDim2.new(0, 0, 0, 0)
+menu.BackgroundColor3 = Color3.fromRGB(200, 200, 200)
+menu.Active = true
+menu.Draggable = true
+menu.Parent = gui
+
+
+local text = Instance.new("TextLabel")
+text.TextSize = 15
+text.Position = UDim2.new(0, 110, 0, 20)
+text.Text = "MrAlikSus's executor"
+text.BackgroundTransparency = 1
+text.TextColor3 = Color3.fromRGB(0, 0, 0)
+text.Parent = menu
+
+local textbox = Instance.new("TextBox")
+textbox.Size = UDim2.new(0, 380, 0, 230)
+textbox.Position = UDim2.new(0, 10, 0, 60)
+textbox.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+textbox.Active = true
+textbox.Parent = menu
+textbox.TextSize = 20
+textbox.TextXAlignment = "Left"
+textbox.TextYAlignment = "Top"
+textbox.Font = "Code"
+textbox.PlaceholderText = "Вставьте ваш текст"
+textbox.TextColor3 = Color3.fromRGB(178, 178, 178)
+textbox.ClearTextOnFocus = false
+textbox.Text = ""
+textbox.TextWrapped = true
+textbox.MultiLine = true
+
+
+local execute = Instance.new("TextButton")
+execute.Size = UDim2.new(0, 100, 0, 50)
+execute.Position = UDim2.new(0, 0, 0, 305)
+execute.BackgroundColor3 = Color3.fromRGB(200, 200, 200)
+execute.Text = "Execute"
+execute.Active = true
+execute.Parent = menu
+
+local clear = Instance.new("TextButton")
+clear.Size = UDim2.new(0, 100, 0, 50)
+clear.Position = UDim2.new(0, 110, 0, 305)
+clear.BackgroundColor3 = Color3.fromRGB(200, 200, 200)
+clear.Text = "Clear"
+clear.Active = true
+clear.Parent = menu
+
+
+local corner1 = Instance.new("UICorner")
+corner1.CornerRadius = UDim.new(0, 5)
+corner1.Parent = menu
+
+local corner2 = Instance.new("UICorner")
+corner2.CornerRadius = UDim.new(0, 5)
+corner2.Parent = textbox
+
+local corner3 = Instance.new("UICorner")
+corner3.CornerRadius = UDim.new(0, 5)
+corner3.Parent = execute
+
+local corner4 = Instance.new("UICorner")
+corner4.CornerRadius = UDim.new(0, 5)
+corner4.Parent = clear
+
+
+
+local menuu = menu
+local textBox = textbox
+local executeButton = execute
+local clearr = clear
+
+local function executeScript()
+    local scriptText = textBox.Text
+    
+    -- Запускаем скрипт, введенный в текстовое поле
+    local success, result = pcall(function()
+        return loadstring(scriptText)()
+    end)
+    
+    -- Если скрипт завершился с ошибкой, выводим сообщение об ошибке
+    if not success then
+        warn("Error executing script: " .. result)
+    end
+end
+
+executeButton.MouseButton1Click:Connect(executeScript)
+
+
+local function clearText()
+    game.CoreGui.ScreenGui.Frame.TextBox.Text = ""
+end
+
+clearr.MouseButton1Click:Connect(clearText)
