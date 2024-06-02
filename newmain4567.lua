c1c1486b283a935fa1b9774fe1dfb572c7cef3df
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-02 12:49:42 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -9,6 +9,26 @@ local Players = game:GetService("Players")
 
 local Player = Players.LocalPlayer
 
+local Dozer = {
+    Name = "Dozer",
+    Themes = {
+        Dark = {
+            ["Main"] = Color3.fromRGB(16,16,16),
+            ["Stroke"] = Color3.fromRGB(174,174,174),
+        }
+    }
+}
 
 --/° Functions °/--
+
+local function Create(Name, Properties, Children)
+	local Object = Instance.new(Name)
+	for i, v in next, Properties or {} do
+		Object[i] = v
+	end
+	for i, v in next, Children or {} do
+		v.Parent = Object
+	end
+	return Object
+end
 
