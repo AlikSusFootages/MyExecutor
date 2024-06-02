36f9981919ce68b7cfc407bc97268bc92dc45a70
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-02 12:46:59 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -2,3 +2,13 @@
 --/° Services °/--
 
 local TweenService = game:GetService("TweenService")
+local Players = game:GetService("Players")
+
+
+--/° Variables °/--
+
+local Player = Players.LocalPlayer
+
+
+--/° Functions °/--
+
