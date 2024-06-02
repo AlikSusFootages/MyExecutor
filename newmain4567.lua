f2888d7fbe96ed7dd0c475df8f04adc408d3b6a8
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-02 17:54:23 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -124,7 +124,7 @@ if game:isLoaded() then
                     BackgroundTransparency = 1,
                     AnchorPoint = Vector2.new(1,0),
                     Size = UDim2.fromOffset(30,30),
-                    Position = UDim2.new(1,0,0,30),
+                    Position = UDim2.new(1,0,0,36),
                     Name = "CloseButton"
                 })
             })
