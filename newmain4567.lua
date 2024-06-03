05cf28a4da91bd48d695ebe59e01db2e87df4ceb
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-03 14:03:03 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -496,4 +496,11 @@ if game:isLoaded() then
         end
     end
     
+    local Sidebar = ScreenGui.Frame.Sidebar
+    
+    require(13699518434):BlurFrame(Sidebar)
+    for i,v in next, RightFrames:GetChildren() do
+        require(13699518434):BlurFrame(v)
+    end
+    
 end
