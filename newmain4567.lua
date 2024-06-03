cb6b168b1d2f75c6c5a8075d6abbd1db87a33aec
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-03 14:18:59 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -496,19 +496,12 @@ if game:isLoaded() then
         end
     end
     
-    local ModuleScript = Create("ModuleScript", {
-        Name = "Blur",
-        Source = [[
-local Sidebar = script.Parent.Frame.Sidebar
-local RightFrames = script.Parent.Frame.RightFrames
-
-require(13699518434):BlurFrame(Sidebar)
-for i,v in next, RightFrames:GetChildren() do
-    require(13699518434):BlurFrame(v)
-end
-        ]],
-        Parent = ScreenGui
-    })
+    local Sidebar = script.Parent.Frame.Sidebar
+    local RightFrames = script.Parent.Frame.RightFrames
     
+    require(13699518434):BlurFrame(Sidebar)
+    for i,v in next, RightFrames:GetChildren() do
+        require(13699518434):BlurFrame(v)
+    end
     
 end
