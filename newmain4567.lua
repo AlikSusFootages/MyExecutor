5c82d95b85fb9fe7cfffb51ebd3e97db56f836f5
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-06 21:42:33 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -727,7 +727,6 @@ function Start()
                     Size = UDim2.new(1,0,1,-20),
                     Position = UDim2.new(0,0,1,0),
                     AnchorPoint = Vector2.new(0,1),
-                    
                 })
             })
         end
