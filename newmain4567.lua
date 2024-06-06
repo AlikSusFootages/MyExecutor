d6b4a2618e510d2f26d536aad3d22e08cbab155b
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-06 20:47:54 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -494,7 +494,9 @@ function Start()
                                 Create("Frame", {
                                     Size = UDim2.new(1,0,1,-36),
                                     Position = UDim2.fromScale(0,1),
-                                    AnchorPoint = Vector2.new(0,1)
+                                    AnchorPoint = Vector2.new(0,1),
+                                    BackgroundTransparency = 1,
+                                    Name = "Content"
                                 })
                             })
                         })
