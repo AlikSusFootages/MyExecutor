858ab2dbd4bbf8f578a6167d6f38988287fd834b
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-06 22:23:51 +0500

    update changelog.lua

--- changelog.lua
+++ changelog.lua
@@ -1,4 +1,24 @@
 return {
+    ["0.0.4"] = {
+        Title = "Release!",
+        Content = "Test example",
+        Date = "06/06/2024"
+    },
+    ["0.0.5"] = {
+        Title = "Release!",
+        Content = "Test example",
+        Date = "06/06/2024"
+    },
+    ["0.0.6"] = {
+        Title = "Release!",
+        Content = "Test example",
+        Date = "06/06/2024"
+    },
+    ["0.0.7"] = {
+        Title = "Release!",
+        Content = "Test example",
+        Date = "06/06/2024"
+    },
     ["0.0.8"] = {
         Title = "Release!",
         Content = "Test example",
