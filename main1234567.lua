86282e5b300c0877fedb0ee2f4e6877c1de0dfbb
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-07 20:00:02 +0500

    update main1234567.lua

--- main1234567.lua
+++ main1234567.lua
@@ -1,6 +1,6 @@
 
 local ColorsLoadstring = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlikSusFootages/Tailwindcss-Roblox/main/colors.lua"))()
-local NewGui = require("newmain4567.lua")
+local NewGui = require("newmain4567")
 
 print("Started")
 
@@ -165,5 +165,5 @@ paste.MouseButton1Click:Connect(function()
 end)
 
 newgui.MouseButton1Click:Connect(function()
-    NewGui()
+    NewGui:Start()
 end)
