c6d83071d0d103da5a8a3625200ee2991210dbab
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-08 21:02:44 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -34,7 +34,7 @@ local Dozer = {
             [[:Connect]], [[:WaitForChild]], [[:FindFirstChild]], [[:GetService]], [[:GetDescendants]], [[:GetChildren]], [[:GetPropertyChangedSignal]]
         },
         [Color3.fromRGB(132, 214, 247)] = {
-            [[new]], [[game]], [[ipairs]], [[pairs]], [[script]], [[wait]], [[math]], [[random]], [[Instance]], [[Parent]], [[task]], [[]]
+            [[new]], [[game]], [[ipairs]], [[pairs]], [[script]], [[wait]], [[math]], [[random]], [[Instance]], [[Parent]], [[task]], [[table]], [[remove]]
         },
         --     "%-%-(.-)"
         -- },
