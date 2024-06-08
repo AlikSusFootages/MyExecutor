014ae97d780a03adc6df9aae3ad7130c35864eff
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-08 22:09:24 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -941,7 +941,7 @@ function Dozer:Start()
         end
         
         local ExecutorButtonsPadding = 5
-        local ExecutorButtonsSize = ExecutorButtons.Size.X.Offset / buttonCount - ExecutorButtonsPadding * buttonCount
+        local ExecutorButtonsSize = ExecutorButtons.AbsoluteSize.X / buttonCount - ExecutorButtonsPadding * buttonCount
         for _, button in ipairs(ExecutorButtons:GetChildren()) do
             if button:IsA("TextButton") then
                 button.Size = UDim2.fromOffset(ExecutorButtonsSize, 0)
