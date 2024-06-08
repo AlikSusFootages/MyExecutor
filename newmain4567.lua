a5ac2ca7a67bc7fbda423c60806c755f2609ffaa
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-08 22:59:45 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -985,7 +985,7 @@ function Dozer:Start()
             Executorr.ScrollingFrame.TextBox.Text = ""
         end)
         ExecutorButtons.Clear.MouseEnter:Connect(function()
-            TweenService:Create(ExecutorButtons.Clear, TweenInfo.new(0.15), {BackgroundTransparency = 0.2}):Play()
+            TweenService:Create(ExecutorButtons.Clear, TweenInfo.new(0.15), {BackgroundTransparency = 0.05}):Play()
         end)
         ExecutorButtons.Clear.MouseLeave:Connect(function()
             TweenService:Create(ExecutorButtons.Clear, TweenInfo.new(0.15), {BackgroundTransparency = 1}):Play()
