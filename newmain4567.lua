25f72ed791b9f991657d5f959e4307017ae86fe6
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-08 22:22:59 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -943,7 +943,7 @@ function Dozer:Start()
         end
         
         local ExecutorButtonsPadding = 10
-        local ExecutorButtonsSize = (ExecutorButtons.AbsoluteSize.X / buttonCount)
+        local ExecutorButtonsSize = (ExecutorButtons.AbsoluteSize.X / buttonCount) - ExecutorButtonsPadding
         print(ExecutorButtonsSize)
         for _, button in ipairs(ExecutorButtons:GetChildren()) do
             if button:IsA("TextButton") then
