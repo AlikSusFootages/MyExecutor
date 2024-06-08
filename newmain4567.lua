66b9cc5d15f879e650729c05eb00a3cb138e0089
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-08 22:10:16 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -632,7 +632,7 @@ function Dozer:Start()
                                 })
                             }),
                             Create("Frame", {
-                                Size = UDim2.new(1,0,0,55),
+                                Size = UDim2.new(1,0,0,50),
                                 BackgroundTransparency = 1,
                                 AnchorPoint = Vector2.new(0,1),
                                 Position = UDim2.new(0,0,1,0)
