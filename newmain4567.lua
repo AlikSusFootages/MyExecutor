c4e2689d7c1db1c3a8b98ec9049442626ba69a21
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-10 19:44:35 +0500

    update newmain4567.lua

--- newmain4567.lua
+++ newmain4567.lua
@@ -1309,8 +1309,8 @@ function Dozer:Start()
                             AutoButtonColor = false,
                             ImageColor3 = UsingTheme.Main,
                             ScaleType = "Slice",
-                            ImageRectOffset = Vector2.new(395,395),
-                            ImageRectSize = Vector2.new(-70,-70),
+                            ImageRectOffset = Vector2.new(-70,-70),
+                            ImageRectSize = Vector2.new(395,395),
                         }),
                         Create("UICorner", {
                             CornerRadius = UDim.new(0, 6)
@@ -1329,8 +1329,8 @@ function Dozer:Start()
                             AutoButtonColor = false,
                             ImageColor3 = UsingTheme.Main,
                             ScaleType = "Slice",
-                            ImageRectOffset = Vector2.new(395,395),
-                            ImageRectSize = Vector2.new(-70,-70),
+                            ImageRectOffset = Vector2.new(-70,-70),
+                            ImageRectSize = Vector2.new(395,395),
                         }),
                         Create("UICorner", {
                             CornerRadius = UDim.new(0, 6)
